ba784d94eb9b1fb9b9e4173b87cf8f5724c2064a
slothmast3r <oskar.straszynski@gmail.com>
2024-04-26 00:31:44 +0200

    newsletter progress

--- newsletter-fm/tailwind.config.ts
+++ newsletter-fm/tailwind.config.ts
@@ -8,6 +8,21 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      listStyleImage: {
+        checkmark: 'url("/assets/images/icon-list.svg")',
+      },
+    colors: {
+      tomato: 'hsl(4, 100%, 67%)',
+      'dark-slate-grey': 'hsl(234, 29%, 20%)',
+      'charcoal-grey': 'hsl(235, 18%, 26%)',
+      'grey': 'hsl(231, 7%, 60%)',
+      // white: 'hsl(0, 0%, 100%)',
+
+    },
+    fontFamily: {
+      roboto: ['Roboto', 'sans-serif'],
+      'sans': ['Roboto', 'Helvetica', 'Arial', 'sans-serif']
+    },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
